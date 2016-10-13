345532087412c41dd88d27e1a55e130541d716e3
Luke Shiner <luke@lukeshiner.com>
2016-10-13 15:57:47 +0100

    Adds robots.txt

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
-from home import views
+from . import views
 
 app_name = 'home'
 urlpatterns = [
+    url(r'^robots.txt$', TemplateView.as_view(
+        template_name='home/robots.txt', content_type='text/plain')),
     url(r'^login/$', views.login_user, name='login_user'),
     url(r'^logout/$', views.logout_user, name='logout_user'),
     url(
