ba1922f79cdcae91f04a8836d4d93c541115f5e8
Luke Shiner <luke@lukeshiner.com>
2017-01-09 13:53:16 +0000

    Starts tests for labelmaker

--- labelmaker/tests.py
+++ labelmaker/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class LabelMakerTemplateTest(TestCase):
+
+    def template_test(self, url, template):
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, template)
+
+    def test_uses_index_template(self):
+        self.template_test('/labelmaker', 'labelmaker/index.html')
+
+    def test_uses_size_chart_form(self):
+        self.template_test(
+            '/create_size_chart_form/', 'labelmaker/size_chart_form.html')
+
+        self.template_test(
+            '/edit_size_chart_form/', 'labelmaker/size_chart_form.html')
