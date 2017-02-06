2e37338b3e99887ac4957ae2cf40892faa0b7258
Luke Shiner <luke@lukeshiner.com>
2017-02-06 12:20:12 +0000

    Updates static and media paths

--- stcadmin/settings.py
+++ stcadmin/settings.py
@@ -125,10 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'public', 'static')
+STATIC_ROOT = os.path.join(
+    os.path.abspath(os.path.dirname(BASE_DIR)), 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'public', 'media')
+MEDIA_ROOT = os.path.join(os.path.abspath(
+    os.path.dirname(BASE_DIR)), 'media')
 
 LOGIN_URL = '/login/'
 
