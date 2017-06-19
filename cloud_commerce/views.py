9bfd523ab8a871e4b5c822486117efe258c49c03
Luke Shiner <luke@lukeshiner.com>
2017-06-19 15:13:15 +0100

    Refactor new product form views.

--- cloud_commerce/views.py
+++ cloud_commerce/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.generic.edit import FormView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from . forms import NewSingleProductForm, NewVariationProductForm
 
@@ -27,20 +28,11 @@ def new_product(request):
     return render(request, 'cloud_commerce/new_product.html')
 
 
-class NewSingleProductView(FormView):
+class NewProductView(FormView):
     template_name = 'cloud_commerce/new_product_form.html'
-    form_class = NewSingleProductForm
     success_url = reverse_lazy('cloud_commerce:index')
 
-    def form_valid(self, form):
-        CCAPI.create_session(settings.CC_LOGIN, settings.CC_PWD)
-        self.create_range(form)
-        self.create_product(form)
-        self.create_options(form)
-        return new_product_success(self.request, self.range, self.product)
-
-    def create_range(self, form):
-        range_name = form.cleaned_data['title'].strip()
+    def create_range(self, range_name, options):
         range_id = CCAPI.create_range(range_name)
         self.range = CCAPI.get_range(range_id)
         while self.range.id == 0:
@@ -48,29 +40,53 @@ class NewSingleProductView(FormView):
             self.range = CCAPI.get_range(range_id)
         self.range = CCAPI.get_range(range_id)
         self.range.id = range_id
+        for option in options:
+            self.range.add_product_option(option)
 
-    def create_product(self, form):
-        name = form.cleaned_data['title'].strip()
-        barcode = form.cleaned_data['barcode'].strip()
-        description = form.cleaned_data['description'].strip()
+    def create_product(
+            self, name, barcode, description, vat_rate_id, weight, height,
+            length, width, large_letter_compatible, stock_level, price,
+            options):
         if len(description) == 0:
             description = name
-        vat_rate_id = int(form.cleaned_data['vat_rate'])
-        self.product = self.range.add_product(
+        product = self.range.add_product(
             name, barcode, description=description, vat_rate_id=vat_rate_id)
-        large_letter_compatible = form.cleaned_data[
-            'package_type'] == 'Large Letter'
-        self.product.set_product_scope(
-            weight=form.cleaned_data['weight'],
-            height=form.cleaned_data['height'],
-            length=form.cleaned_data['length'],
-            width=form.cleaned_data['width'],
-            large_letter_compatible=large_letter_compatible)
-        self.product.set_stock_level(form.cleaned_data['stock_level'])
-        self.product.set_handling_time(1)
-        self.product.set_base_price(form.cleaned_data['price'])
+        product.set_product_scope(
+            weight, height, length, width, large_letter_compatible)
+        product.set_stock_level(stock_level)
+        product.set_handling_time(1)
+        product.set_base_price(price)
+        for option, value in options.items():
+            if len(value) > 0:
+                product.set_option_value(option, value, create=True)
+        return product
 
-    def create_options(self, form):
+
+class NewSingleProductView(LoginRequiredMixin, NewProductView):
+    form_class = NewSingleProductForm
+
+    def form_valid(self, form):
+        data = form.cleaned_data
+        CCAPI.create_session(settings.CC_LOGIN, settings.CC_PWD)
+        options = self.get_options(form)
+        self.create_range(data['title'].strip(), options.keys())
+        large_letter_compatible = options['Package Type'] == 'Large Letter'
+        self.product = self.create_product(
+            name=data['title'].strip(),
+            barcode=data['barcode'].strip(),
+            description=data['description'].strip(),
+            vat_rate_id=int(data['vat_rate']),
+            weight=data['weight'],
+            height=data['height'],
+            length=data['length'],
+            width=data['width'],
+            large_letter_compatible=large_letter_compatible,
+            stock_level=data['stock_level'],
+            price=data['price'],
+            options=options)
+        return new_product_success(self.request, self.range, self.product)
+
+    def get_options(self, form):
         required_options = {
             'Department': form.cleaned_data['department'].strip(),
             'Brand': form.cleaned_data['brand'].strip(),
@@ -84,17 +100,11 @@ class NewSingleProductView(FormView):
             form.cleaned_data.items() if key.startswith('opt_') and
             len(value.strip()) > 0}
         options = {**required_options, **optional_options}
-        for option in options:
-            self.range.add_product_option(option)
-        for option, value in options.items():
-            if len(value) > 0:
-                self.product.set_option_value(option, value, create=True)
+        return options
 
 
-class NewVariationProductView(FormView):
-    template_name = 'cloud_commerce/new_product_form.html'
+class NewVariationProductView(LoginRequiredMixin, NewProductView):
     form_class = NewVariationProductForm
-    success_url = reverse_lazy('cloud_commerce:index')
 
     def form_valid(self, form):
         return super(NewVariationProductView, self).form_valid(form)
