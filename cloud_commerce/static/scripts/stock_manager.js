942011ba9e346b7fcf0510eb41d7329d7264782d
Luke Shiner <luke@lukeshiner.com>
2017-06-19 12:06:06 +0100

    Refactor stock_manager.js

--- cloud_commerce/static/scripts/stock_manager.js
+++ cloud_commerce/static/scripts/stock_manager.js
@@ -13,22 +13,29 @@ $(document).ready(function() {
     function update_stock_button(product_id) {
         return function (event) {
             var stock_input = $('#stock_' + product_id);
-            $.ajax({
-                type: 'POST',
-                url: update_stock_url,
-                data: JSON.stringify({
-                    'product_id': product_id,
-                    'new_stock_level': stock_input.val(),
-                    'old_stock_level': product_details[product_id].stock_level,
-                }),
-                success: function(response) {
-                    console.log(response);
-                    product_details[product_id].stock_level = response;
-                    $('#stock_' + product_id).val(response);
-                },
-                contentType: "application/json",
-                dataType: 'json'
-            });
+            if (stock_input.val() != product_details[product_id].stock_level) {
+                update_stock(product_id);
+            }
         }
+    }
+
+    function update_stock(product_id) {
+        var stock_input = $('#stock_' + product_id);
+        $.ajax({
+            type: 'POST',
+            url: update_stock_url,
+            data: JSON.stringify({
+                'product_id': product_id,
+                'new_stock_level': stock_input.val(),
+                'old_stock_level': product_details[product_id].stock_level,
+            }),
+            success: function(response) {
+                console.log(response);
+                product_details[product_id].stock_level = response;
+                $('#stock_' + product_id).val(response);
+            },
+            contentType: "application/json",
+            dataType: 'json'
+        });
     }
 });
