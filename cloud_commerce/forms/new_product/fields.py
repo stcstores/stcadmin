4cd5df6e1b3bdda9658d9ffd911f6fd2f67a1393
Luke Shiner <luke@lukeshiner.com>
2017-06-22 11:37:27 +0100

    Refactor new product fields.

--- cloud_commerce/forms/new_product/fields.py
+++ cloud_commerce/forms/new_product/fields.py
@@ -8,116 +8,293 @@ from ccapi import CCAPI
 CCAPI.create_session(settings.CC_LOGIN, settings.CC_PWD)
 
 
-OPTIONS = [
-    option.option_name for option in CCAPI.get_product_options() if
-    option.exclusions['tesco'] is False]
+class MetaFormFields(type):
 
-PACKAGE_TYPES = [
-    (service.value, service.value)
-    for service in CCAPI.get_option_values("33852")]
-
-DEPARTMENTS = [
-    (dept.value, dept.value) for dept in CCAPI.get_option_values("34325")]
-
-VAT_RATES = ([
-    (5, 'Normal Rate 20%'), (2, 'Reduced 5%'), (1, 'VAT Exempt')])
-
-SUPPLIERS = [
-    (supplier.value, supplier.value) for supplier in CCAPI.get_option_values(
-        "35131")]
-SUPPLIERS.sort(key=lambda x: x[1])
+    def __iter__(self):
+        for field in self.fields:
+            yield field
 
 
-class NewProductFormField:
+class MetaFormField(type):
 
+    @property
+    def is_required(self):
+        if len(self.required_message) > 0:
+            return True
+        return False
+
+
+class FormField(metaclass=MetaFormField):
+
+    label = None
+    name = None
+    field = None
+    variable = False
+    variation = False
+    option = False
+    help_text = None
+    placeholder = None
+    html_class = None
     size = 50
+    initial = None
+    required_message = ''
+    option = False
 
-    def __new__(
-            self, required_message=None, size=None, placeholder=None,
-            html_class='new_product_input', textarea=False, label=None,
-            initial=None, help_text=None):
-        if size is None:
-            size = self.size
-        if textarea is True:
-            widget_class = forms.Textarea
-        else:
-            widget_class = forms.TextInput
-        if required_message is not None:
-            required = True
-            error_messages = {'required': required_message}
-        else:
-            required = False
-            error_messages = {}
+    @classmethod
+    def get_widget(cls):
         attrs = {}
-        if placeholder is not None:
-            attrs['placeholder'] = placeholder
-        if html_class is not None:
-            attrs['class'] = html_class
-        if size is not None:
-            attrs['size'] = size
-        return self.field_class(
-            required=required, label=label, error_messages=error_messages,
-            widget=widget_class(attrs=attrs), initial=initial,
-            help_text=help_text)
+        if cls.placeholder is not None:
+            attrs['placeholder'] = cls.placeholder
+        if cls.html_class is not None:
+            attrs['class'] = cls.html_class
+        if cls.size is not None:
+            attrs['size'] = cls.size
+        return cls.widget_class(attrs=attrs)
+
+    @classmethod
+    def field(cls):
+        error_messages = None
+        if cls.is_required:
+            error_messages = {'required': cls.required_message}
+        widget = cls.get_widget()
+        field = cls.form_class(
+            required=cls.is_required, label=cls.label,
+            error_messages=error_messages,
+            widget=widget, initial=cls.initial,
+            help_text=cls.help_text)
+        return field
 
 
-class TextField(NewProductFormField):
-    field_class = forms.CharField
+class ChoiceField(FormField):
+
+    form_class = forms.ChoiceField
+    widget_class = forms.Select
+
+    @classmethod
+    def get_widget(cls):
+        return cls.widget_class()
+
+    @classmethod
+    def field(self):
+        return self.form_class(
+            choices=self.get_choices(), label=self.label,
+            widget=self.get_widget())
 
 
-class NumberField(NewProductFormField):
+class NumberField(FormField):
 
-    size = 5
-    field_class = forms.IntegerField
-
-
-title = TextField(
-    required_message="Please supply a range title", placeholder='Title')
-description = TextField(
-    placeholder='Description. Will default to title if left blank',
-    textarea=True)
-barcode = TextField(
-    required_message="Please supply a barcode", placeholder='Barcode')
-department = forms.ChoiceField(choices=DEPARTMENTS)
-price = TextField(
-    required_message="Please supply a price",
-    placeholder='Price without shipping or VAT',
-    label='Price (ex VAT)')
-purchase_price = TextField(
-    required_message="Please supply a price", placeholder='Purchase Price')
-stock_level = NumberField(
-    initial=0, label='Stock Level',
-    required_message='Please provide a stock level. This can be zero.')
-vat_rate = forms.ChoiceField(choices=VAT_RATES, label='VAT Rate')
-supplier = forms.ChoiceField(choices=SUPPLIERS)
-supplier_SKU = TextField(placeholder='Supplier SKU', label='Supplier SKU')
-weight = NumberField(
-    label='Weight (Grams)', required_message="Please supply a weight")
-height = NumberField(label='Height (Milimeters)')
-width = NumberField(label='Width (Milimeters)')
-length = NumberField(label='Length (Milimeters)')
-package_type = forms.ChoiceField(choices=PACKAGE_TYPES, label='Package Type')
-location = TextField(placeholder='Location')
-brand = TextField(
-    required_message="Please supply a brand", placeholder='Brand')
-manufacturer = TextField(
-    required_message="Please supply a manufacturer",
-    placeholder='Manufacturer')
+    form_class = forms.IntegerField
+    widget_class = forms.NumberInput
 
 
-class OptionField:
-
-    def __new__(self, name):
-        return TextField(placeholder=name, label=name)
-
-
-option_fields = [(option, OptionField(option)) for option in OPTIONS]
+class PriceField(FormField):
+    form_class = forms.FloatField
+    widget_class = forms.NumberInput
 
 
-class VariationField:
+class TextField(FormField):
+    form_class = forms.CharField
+    widget_class = forms.TextInput
 
-    def __init__(self, title, field, variable=False, variation=False):
-        self.title = title
-        self.field = field
-        self.variable = variable
-        self.variation = variation
+
+class TextareaField(FormField):
+    form_class = forms.CharField
+    widget_class = forms.Textarea
+
+
+class OptionField(TextField):
+
+    option = True
+    variable = True
+    variation = True
+
+    def __init__(self, option):
+        self.label = option
+        self.name = 'opt_{}'.format(option)
+
+    def select_field(self):
+        choices = [
+            ('{}_unused'.format(self.name), 'Unused'.format(self.name)),
+            ('{}_variable'.format(self.name), 'Variable'.format(self.name)),
+            ('{}_variation'.format(self.name), 'Variation'.format(self.name))]
+        field = forms.ChoiceField(
+            label=self.label, widget=forms.RadioSelect,
+            choices=choices, initial='{}_unused'.format(self.name))
+        return field
+
+
+class Title(TextField):
+    label = 'Title'
+    name = 'title'
+    required_message = "Please supply a range title"
+    placeholder = 'Title'
+
+
+class Description(TextareaField):
+    label = 'Description'
+    name = 'description'
+    placeholder = 'Description. Will default to title if left blank'
+
+
+class Barcode(TextField):
+    label = 'Barcode'
+    name = 'barcode'
+    required_message = "Please supply a barcode"
+    placeholder = 'Barcode'
+    variable = True
+
+
+class Department(ChoiceField):
+    label = 'Department'
+    name = 'department'
+    placeholder = 'Description. Will default to title if left blank'
+
+    @staticmethod
+    def get_choices():
+        return [
+            (dept.value, dept.value) for dept in
+            CCAPI.get_option_values("34325")]
+
+
+class Price(PriceField):
+    label = 'Price (ex VAT)'
+    name = 'price'
+    required_message = "Please supply a price"
+    placeholder = 'Price without shipping or VAT'
+    variable = True
+
+
+class PurchasePrice(PriceField):
+    label = 'Purchase Price'
+    name = 'purchase_price'
+    initial = 0,
+    required_message = 'Please provide a stock level. This can be zero.'
+    variable = True
+
+
+class StockLevel(NumberField):
+    label = 'Stock Level'
+    name = 'stock_level'
+    initial = 0
+    required_message = 'Please provide a stock level. This can be zero.'
+    variable = True
+
+
+class VATRate(ChoiceField):
+    label = 'VAT Rate'
+    name = 'vat_rate'
+    variable = True
+
+    @staticmethod
+    def get_choices():
+        return ([
+            (5, 'Normal Rate 20%'), (2, 'Reduced 5%'), (1, 'VAT Exempt')])
+
+
+class Supplier(ChoiceField):
+    label = 'Supplier'
+    name = 'supplier'
+
+    @staticmethod
+    def get_choices():
+        suppliers = [
+            (supplier.value, supplier.value) for supplier in
+            CCAPI.get_option_values("35131")]
+        suppliers.sort(key=lambda x: x[1])
+        return suppliers
+
+
+class SupplierSKU(TextField):
+    label = 'Supplier SKU'
+    name = 'supplier_SKU'
+    placeholder = 'Supplier SKU'
+    variable = True
+
+
+class Weight(NumberField):
+    label = 'Weight (Grams)'
+    name = 'weight'
+    required_message = "Please supply a weight"
+    variable = True
+
+
+class Height(NumberField):
+    label = 'Height (Milimeters)'
+    name = 'height'
+    variable = True
+
+
+class Width(NumberField):
+    label = 'Width (Milimeters)'
+    name = 'width'
+    variable = True
+
+
+class Length(NumberField):
+    label = 'Length (Milimeters)'
+    name = 'length'
+    variable = True
+
+
+class PackageType(ChoiceField):
+    label = 'Package Type'
+    name = 'package_type'
+    variable = True
+
+    @staticmethod
+    def get_choices():
+        return [
+            (service.value, service.value)
+            for service in CCAPI.get_option_values("33852")]
+
+
+class Location(TextField):
+    label = 'Location'
+    name = 'location'
+    placeholder = 'Location'
+    variable = True
+
+
+class Brand(TextField):
+    label = 'Brand'
+    name = 'brand'
+    required_message = "Please supply a brand"
+    placeholder = 'Brand'
+
+
+class Manufacturer(TextField):
+    label = 'Manufacturer'
+    name = 'manufacturer'
+    required_message = "Please supply a manufacturer",
+    placeholder = 'Manufacturer'
+
+
+class FormFields(metaclass=MetaFormFields):
+    fields = [
+        Title,
+        Description,
+        Barcode,
+        Department,
+        Price,
+        PurchasePrice,
+        StockLevel,
+        VATRate,
+        Supplier,
+        SupplierSKU,
+        Weight,
+        Height,
+        Width,
+        Length,
+        PackageType,
+        Location,
+        Brand,
+        Manufacturer,
+        ]
+
+    @staticmethod
+    def get_options():
+        return [
+            OptionField(option.option_name) for option in
+            CCAPI.get_product_options() if option.exclusions['tesco'] is False]
+
+    fields += get_options.__func__()
--- cloud_commerce/forms/new_product/forms.py
+++ cloud_commerce/forms/new_product/forms.py
@@ -3,7 +3,7 @@ from django.forms import formset_factory
 
 import json
 
-from . import fields
+from . fields import FormFields
 from . import widgets
 
 
@@ -13,33 +13,14 @@ class NewProductForm(forms.Form):
 
 class NewSingleProductForm(NewProductForm):
 
-    title = fields.title
-    description = fields.description
-    barcode = fields.barcode
-    department = fields.department
-    price = fields.price
-    purchase_price = fields.purchase_price
-    package_type = fields.package_type
-    vat_rate = fields.vat_rate
-    stock_level = fields.stock_level
-    brand = fields.brand
-    manufacturer = fields.manufacturer
-    supplier = fields.supplier
-    supplier_SKU = fields.supplier_SKU
-    weight = fields.weight
-    length = fields.length
-    height = fields.height
-    width = fields.width
-    location = fields.location
-
     def __init__(self, *args, **kwargs):
-        super(NewSingleProductForm, self).__init__(*args, **kwargs)
-        for option, field in fields.option_fields:
-            self.fields['opt_{}'.format(option)] = field
+        super().__init__(*args, **kwargs)
+        for field in FormFields:
+            self.fields[field.name] = field.field()
 
 
 class NewVariationProductForm(NewProductForm):
-
+    """
     setup_fields = [
         fields.VariationField('title', fields.title),
         fields.VariationField('description', fields.description),
@@ -63,32 +44,58 @@ class NewVariationProductForm(NewProductForm):
         fields.VariationField('height', fields.height, variable=True),
         fields.VariationField('width', fields.width, variable=True),
         fields.VariationField('location', fields.location, variable=True),
-        ]
+        ]"""
 
     def __init__(self, *args, **kwargs):
-        super(NewVariationProductForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.create_fields()
 
     def create_fields(self):
-        [self.create_field(field) for field in self.setup_fields]
-        for option, field in fields.option_fields:
-            choices = [
-                ('{}_unused'.format(option), 'Unused'.format(option)),
-                ('{}_variable'.format(option), 'Variable'.format(option)),
-                ('{}_variation'.format(option), 'Variation'.format(option))]
-            self.fields['opt_' + option] = forms.ChoiceField(
-                label=option, choices=choices, widget=forms.RadioSelect,
-                initial='{}_unused'.format(option))
-        self.options = [option for option, field in fields.option_fields]
+        [self.create_field(field) for field in FormFields
+            if field.option is False]
+        [self.create_field(field) for field in FormFields
+            if field.option is True]
+
+    def create_option_field(self, field):
+        choices = [
+            ('{}_unused'.format(field.name), 'Unused'.format(field.name)),
+            ('{}_variable'.format(field.name), 'Variable'.format(field.name)),
+            ('{}_variation'.format(field.name), 'Variation'.format(field.name))
+        ]
+        self.fields['opt_' + field.name] = forms.ChoiceField(
+            label=field.label, choices=choices, widget=forms.RadioSelect,
+            initial='{}_unused'.format(field.name))
 
     def create_field(self, field):
-        self.fields[field.title] = field.field
+        self.fields[field.name] = field.field
         if field.variable:
-            self.fields['variable_' + field.title] = forms.BooleanField(
+            self.fields['variable_' + field.name] = forms.BooleanField(
                 required=False)
         if field.variation:
-            self.fields['variation_' + field.title] = forms.BooleanField(
+            self.fields['variation_' + field.name] = forms.BooleanField(
                 required=False)
+
+
+class TempVariationForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in FormFields:
+            if field.option:
+                self.fields['opt_' + field.name] = field.select_field()
+
+    def clean(self):
+        cleaned_data = super().clean()
+        selected_options = []
+        variable_options = []
+        for key, value in cleaned_data.items():
+            if 'opt_' in key:
+                if 'variation' in value:
+                    selected_options.append(self.fields[key].label)
+                elif 'variable' in value:
+                    variable_options.append(self.fields[key])
+        cleaned_data['selected_options'] = selected_options
+        return cleaned_data
 
 
 class VariationChoicesForm(forms.Form):
@@ -110,33 +117,6 @@ class VariationChoicesForm(forms.Form):
                 cleaned_data[key] = json.loads(value)
             else:
                 cleaned_data[key] = []
-        return cleaned_data
-
-
-class TempVariationForm(forms.Form):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for option, field in fields.option_fields:
-            choices = [
-                ('{}_unused'.format(option), 'Unused'.format(option)),
-                ('{}_variable'.format(option), 'Variable'.format(option)),
-                ('{}_variation'.format(option), 'Variation'.format(option))]
-            self.fields['opt_' + option] = forms.ChoiceField(
-                label=option, choices=choices, widget=forms.RadioSelect,
-                initial='{}_unused'.format(option))
-
-    def clean(self):
-        cleaned_data = super().clean()
-        selected_options = []
-        variable_options = []
-        for key, value in cleaned_data.items():
-            if 'opt_' in key:
-                if 'variation' in value:
-                    selected_options.append(self.fields[key].label)
-                elif 'variable' in value:
-                    variable_options.append(self.fields[key])
-        cleaned_data['selected_options'] = selected_options
         return cleaned_data
 
 
