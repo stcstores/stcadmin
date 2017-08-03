3f2a4b965f2eccbc45e2a7014dc932d0f175fbf2
Luke Shiner <luke@lukeshiner.com>
2017-08-03 10:51:47 +0100

    Update password validation.

--- stcadmin/settings.py
+++ stcadmin/settings.py
@@ -95,12 +95,6 @@ WSGI_APPLICATION = 'stcadmin.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
