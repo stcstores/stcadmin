4f41030cd3ef29ef2059392853743a854d138574
Luke Shiner <luke@lukeshiner.com>
2017-09-04 17:02:24 +0100

    Add charts to navigation and colours orders by day.

--- print_audit/views/charts.py
+++ print_audit/views/charts.py
@@ -19,20 +19,51 @@ class GetChart(PrintAuditUserMixin, View):
         orders = models.CloudCommerceOrder.objects.all().annotate(
             date=TruncDate('date_created')).values('date').annotate(
                 count=Count('pk')).order_by('date')
-        return make_chart(orders, 'barh')
+        return self.make_chart(orders, 'barh')
 
+    def get_days(self, orders):
+        class Day:
+            def __init__(self, name, index, colour):
+                self.name = name
+                self.colour = colour
+                self.index = index
+                self.x = []
+                self.y = []
 
-def make_chart(orders, chart_type='bar'):
-    x = [order['date'] for order in orders]
-    y = [order['count'] for order in orders]
-    getattr(plt, chart_type)(x, y)
-    plt.grid(True)
-    plt.style.use('ggplot')
-    plt.title('Orders for Date')
-    plt.xlabel('Date')
-    plt.ylabel('Order Count')
-    html = mpld3.fig_to_html(plt.gcf())
-    return HttpResponse(html)
+            def get_orders(self, orders):
+                orders = (
+                    order for order in orders if
+                    order['date'].weekday() == self.index)
+                for order in orders:
+                    self.x.append(order['date'])
+                    self.y.append(order['count'])
+
+            def make_plot(self, plt):
+                plt(self.x, self.y, label=self.name, color=self.colour)
+
+        days = (
+            Day('Monday', 0, 'r'),
+            Day('Tuesday', 1, 'b'),
+            Day('Wednesday', 2, 'b'),
+            Day('Thursday', 3, 'b'),
+            Day('Friday', 4, 'b'),
+            Day('Saturday', 5, 'g'),
+            Day('Sunday', 6, 'g'))
+        for day in days:
+            day.get_orders(orders)
+        return days
+
+    def make_chart(self, orders, chart_type='bar'):
+        days = self.get_days(orders)
+        for day in days:
+            day.make_plot(getattr(plt, chart_type))
+        plt.grid(True)
+        plt.style.use('ggplot')
+        plt.title('Orders for Date')
+        plt.xlabel('Date')
+        plt.ylabel('Order Count')
+        html = mpld3.fig_to_html(plt.gcf())
+        return HttpResponse(html)
 
 
 class Charts(PrintAuditUserMixin, TemplateView):
