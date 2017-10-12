a1c2f41a260ad8e6e626319c60dd50fb4327cf94
Luke Shiner <luke@lukeshiner.com>
2017-10-12 14:53:18 +0100

    Only use ListWidget from list_input.

--- inventory/forms/new_product/widgets.py
+++ inventory/forms/new_product/widgets.py
@@ -1,21 +1,7 @@
 from django import forms
-from django.forms.utils import flatatt
-from django.utils.safestring import mark_safe
 from django.template.loader import render_to_string
-
-
-class ListWidget(forms.TextInput):
-
-    list_separator = ';'
-
-    def render(self, name, value, attrs):
-        flat_attrs = flatatt(attrs)
-        html = render_to_string(
-            'inventory/widgets/list_widget.html',
-            {
-                'id': attrs['id'], 'attrs': flat_attrs, 'value': value,
-                'name': name})
-        return mark_safe(html)
+from django.utils.safestring import mark_safe
+from list_input import ListWidget
 
 
 class HorizontalRadioRenderer(forms.RadioSelect.renderer):
