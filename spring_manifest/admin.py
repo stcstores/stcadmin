8f74a22b18ed654c15491e4d1719b0fccd176acc
Luke Shiner <luke@lukeshiner.com>
2017-10-24 10:30:06 +0100

    Improvements to spring_manifest admin.

--- spring_manifest/admin.py
+++ spring_manifest/admin.py
@@ -31,21 +31,24 @@ class CloudCommerceCountryIDAdmin(admin.ModelAdmin):
 class SpringManifestAdmin(admin.ModelAdmin):
     fields = ('manifest_type', 'time_filed', 'manifest_file')
     list_display = (
-        'id', 'manifest_type', 'time_created', 'time_filed', 'manifest_file')
-    list_display_links = ('id', )
+        'id', '__str__', 'manifest_type', 'time_created', 'time_filed',
+        'manifest_file')
+    list_display_links = ('__str__', )
     list_editable = ('manifest_type', )
 
 
 @admin.register(models.SpringOrder)
 class SpringOrderAdmin(admin.ModelAdmin):
     fields = (
-        'order_id', 'customer_name', 'date_recieved', 'dispatch_date',
-        'country', 'product_count', 'package_count', 'manifest', 'service')
+        'id', 'order_id', 'customer_name', 'date_recieved', 'dispatch_date',
+        'country', 'product_count', 'package_count', 'manifest', 'service',
+        'canceled')
     list_display = (
-        'order_id', 'customer_name', 'date_recieved', 'dispatch_date',
-        'country', 'product_count', 'package_count', 'manifest', 'service')
-    list_display_links = ('order_id', )
-    list_editable = ('product_count', 'package_count')
+        '__str__', 'order_id', 'customer_name', 'date_recieved',
+        'dispatch_date', 'country', 'product_count', 'package_count',
+        'manifest', 'service', 'canceled')
+    list_display_links = ('__str__', )
+    list_editable = ('product_count', 'package_count', 'canceled')
     list_filter = ('date_recieved', 'dispatch_date', 'service', 'manifest')
 
     def __repr__(self):
