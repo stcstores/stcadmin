2cb3062446a2da50cf2f22188b53d844dd8b3e84
Luke Shiner <luke@lukeshiner.com>
2017-11-16 10:33:52 +0000

    Start using database values for shipping cost in price calculator.

--- inventory/templatetags/inventory_extras.py
+++ inventory/templatetags/inventory_extras.py
@@ -1,5 +1,9 @@
+import json
+
 from ccapi import URLs
 from django import template
+from django.utils.safestring import mark_safe
+from inventory import models
 
 register = template.Library()
 SUBDOMAIN = 'seatontradingcompany'
@@ -31,3 +35,28 @@ def product_options(option=None):
         return {x.option_name: x.values for x in ProductOptions}
     else:
         return ProductOptions[option].options
+
+
+@register.simple_tag
+def shipping_price_data():
+    lines = []
+    shipping_prices = {}
+    for price in models.ShippingPrice.objects.all():
+        if price.country.name not in shipping_prices:
+            shipping_prices[price.country.name] = []
+        shipping_prices[price.country.name].append({
+            'package_type': [p.name for p in price.package_type.all()],
+            'min_weight': price.min_wieght,
+            'max_weight': price.max_wieght,
+            'item_price': price.item_price,
+            'kilo_price': price.kilo_price,
+        })
+    lines.append(
+        'var shipping_prices = {};'.format(json.dumps(shipping_prices)))
+    lines.append(
+        'var countries = {};'.format(json.dumps(
+            [x.name for x in models.DestinationCountry.objects.all()])))
+    lines.append(
+        'var package_types = {};'.format(json.dumps(
+            [x.name for x in models.PackageType.objects.all()])))
+    return mark_safe('\n'.join(lines))
