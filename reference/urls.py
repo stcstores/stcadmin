40ae0c740754b7612d2259e6c6f85746a2a02fc1
Luke Shiner <luke@lukeshiner.com>
2017-12-05 11:21:32 +0000

    Add spring services to reference.

--- reference/urls.py
+++ reference/urls.py
@@ -4,5 +4,8 @@ from reference import views
 app_name = 'reference'
 
 urlpatterns = [
+    url(
+        r'^spring_services$', views.SpringServices.as_view(),
+        name='spring_services'),
     url(r'^$', views.Index.as_view(), name='index'),
 ]
--- reference/views.py
+++ reference/views.py
@@ -3,3 +3,7 @@ from django.views.generic.base import TemplateView
 
 class Index(TemplateView):
     template_name = 'reference/index.html'
+
+
+class SpringServices(TemplateView):
+    template_name = 'reference/spring_services.html'
