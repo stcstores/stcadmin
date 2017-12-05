4f631fb61a3701aa2a767f2fc0f9f2c832b847f5
Luke Shiner <luke@lukeshiner.com>
2017-12-05 16:56:10 +0000

    Add help_text to end of line field in product range form.

--- inventory/forms/forms.py
+++ inventory/forms/forms.py
@@ -5,7 +5,11 @@ from .new_product.fields import Description, Title
 
 
 class ProductRangeForm(forms.Form):
-    end_of_line = forms.BooleanField(required=False)
+    end_of_line = forms.BooleanField(
+        required=False,
+        help_text=(
+            'Ranges are maked as <b>end of line</b> if the entire range is'
+            ' out of stock and unlikely to be re-ordered.'))
 
 
 class DescriptionForm(forms.Form):
