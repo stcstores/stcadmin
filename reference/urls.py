5aeed150614ac2d44f0441131352ddcc666a3251
Luke Shiner <luke@lukeshiner.com>
2017-12-11 15:38:53 +0000

    Use currency rates in spring manifest instead of live conversions.
    Starts product creation instructions.
    
    Continues new product creation instructions.

--- reference/urls.py
+++ reference/urls.py
@@ -5,6 +5,9 @@ app_name = 'reference'
 
 urlpatterns = [
     path(
+        'product_creation', views.ProductCreation.as_view(),
+        name='product_creation'),
+    path(
         'spring_services/', views.SpringServices.as_view(),
         name='spring_services'),
     path('', views.Index.as_view(), name='index'),
--- reference/views.py
+++ reference/views.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 
 
-class Index(TemplateView):
+class Index(LoginRequiredMixin, TemplateView):
     template_name = 'reference/index.html'
 
 
-class SpringServices(TemplateView):
+class SpringServices(LoginRequiredMixin, TemplateView):
     template_name = 'reference/spring_services.html'
+
+
+class ProductCreation(LoginRequiredMixin, TemplateView):
+    template_name = 'reference/product_creation_base.html'
