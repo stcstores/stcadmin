d9de08982bce80e9ac3485c463e778fdd2d7afeb
Luke Shiner <luke@lukeshiner.com>
2018-02-07 10:24:26 +0000

    Add logging to profit_loss management command.

--- profit_loss/management/commands/update_profit_loss.py
+++ profit_loss/management/commands/update_profit_loss.py
@@ -1,10 +1,21 @@
+import logging
+import sys
+
 from django.core.management.base import BaseCommand
 
 from profit_loss.models import UpdateOrderProfit
+
+logger = logging.getLogger('management_commands')
 
 
 class Command(BaseCommand):
     help = 'Updates Print Audit'
 
     def handle(self, *args, **options):
-        UpdateOrderProfit()
+        try:
+            UpdateOrderProfit()
+        except Exception as e:
+            logger.error(
+                'Admin Command Error: %s', ' '.join(sys.argv),
+                exc_info=sys.exc_info())
+            raise e
