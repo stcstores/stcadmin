633dafe9a46442890ebdd345cd5f11f983e39780
Luke Shiner <luke@lukeshiner.com>
2018-05-03 12:48:09 +0100

    Move feedback images to feedback directory.

--- print_audit/models.py
+++ print_audit/models.py
@@ -94,7 +94,7 @@ class Feedback(models.Model):
     """Model for feedback scores."""
 
     name = models.CharField(max_length=20)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='feedback')
     score = models.IntegerField(default=0)
 
     def __str__(self):
