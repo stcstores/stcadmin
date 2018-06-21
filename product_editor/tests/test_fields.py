4ba7b478c596560aefb6ade44100815361b3a73b
Luke Shiner <luke@lukeshiner.com>
2018-06-21 15:34:51 +0100

    Add tests for Description field validation.

--- product_editor/tests/test_fields.py
+++ product_editor/tests/test_fields.py
@@ -9,7 +9,7 @@ class TestField(TestCase):
     """Tests for Product Editor form fields."""
 
     def test_title_disallowed_characters(self):
-        """Test Field class allowed characters."""
+        """Test Title field class allowed characters."""
         field = fields.Title()
         field.validate('Hello World')
         with self.assertRaises(ValidationError):
@@ -18,7 +18,17 @@ class TestField(TestCase):
     def test_option_field_allowed_characters(self):
         """Test option field allowed characters."""
         field = fields.VariationOptions(label='Size', choices=[])
-        field = fields.Title()
         field.validate('Hello World')
+        field.validate('Hello + (world) .')
         with self.assertRaises(ValidationError):
-            field.validate('Hello + (world) .')
+            field.validate('Hello ~ world')
+
+    def test_description_disallowed_characters(self):
+        """Test Description field class allowed characters."""
+        field = fields.Description()
+        field.validate('Hello World')
+        field.validate('Hello + (world) .')
+        with self.assertRaises(ValidationError):
+            field.validate('hello~world')
+        with self.assertRaises(ValidationError):
+            field.validate('hello ~ world')
