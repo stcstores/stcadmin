00a744998f2a7c0504297b81ffa2ff2b3cd8a360
Luke Shiner <luke@lukeshiner.com>
2018-06-21 13:46:50 +0100

    Fix issues in labelmaker tests.

--- labelmaker/tests.py
+++ labelmaker/tests.py
@@ -1,24 +1,27 @@
 """Tests for labelmaker app."""
 
+from django.contrib.auth.models import Group, User
 from django.test import TestCase
 
 
 class LabelMakerTemplateTest(TestCase):
     """Tests for labelmaker templates."""
 
+    GROUP = 'labelmaker'
+
+    def setUp(self):
+        """Login user."""
+        user = User.objects.create_user('foo', 'myemail@test.com', 'bar')
+        self.client.login(username='foo', password='bar')
+        group = Group.objects.get(name=self.GROUP)
+        group.user_set.add(user)
+
     def template_test(self, url, template):
         """Test url uses template."""
         response = self.client.get(url)
+        self.failUnlessEqual(response.status_code, 200)
         self.assertTemplateUsed(response, template)
 
     def test_uses_index_template(self):
         """Test labelmaker index page uses the correct template."""
-        self.template_test('/labelmaker', 'labelmaker/index.html')
-
-    def test_uses_size_chart_form(self):
-        """Test size chart pages use size chart form template."""
-        self.template_test(
-            '/create_size_chart_form/', 'labelmaker/size_chart_form.html')
-
-        self.template_test(
-            '/edit_size_chart_form/', 'labelmaker/size_chart_form.html')
+        self.template_test('/labelmaker/', 'labelmaker/index.html')
