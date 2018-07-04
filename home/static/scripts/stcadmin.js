4fb85254d2de022f2a5bfdea0a46974e5caf35ac
Luke Shiner <luke@lukeshiner.com>
2018-07-04 11:05:56 +0100

    Set datefield widget in stcadmin.js.

--- home/static/scripts/stcadmin.js
+++ home/static/scripts/stcadmin.js
@@ -1,3 +1,11 @@
 function api_error() {
     alert('An error occured.\nPlease check if your operation completed successfully.\nIf not please try again.')
 }
+
+$(document).ready(function() {
+  $('.datepicker').datepicker({
+    showOtherMonths: true,
+    selectOtherMonths: true,
+    dateFormat: 'yy-mm-dd'
+  });
+});
