e244f5085cd3dbbbb5740a9869078f1c3131a2cb
Luke Shiner <luke@lukeshiner.com>
2018-07-19 16:09:30 +0100

    Add logging to update_print_audit.

--- print_audit/management/commands/update_print_audit.py
+++ print_audit/management/commands/update_print_audit.py
@@ -1,9 +1,13 @@
 """Management commands for the Print Audit app."""
 
+import logging
+import sys
+
+from ccapi import CCAPI
 from django.core.management.base import BaseCommand
 from print_audit.models import CloudCommerceOrder
 
-from ccapi import CCAPI
+logger = logging.getLogger('management_commands')
 
 
 class Command(BaseCommand):
@@ -13,12 +17,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Update print audit."""
-        print_logs = CCAPI.get_print_queue()
-        new_order_count = 0
-        for print_log in print_logs:
-            order_exists = CloudCommerceOrder.objects.filter(
-                order_id=print_log.order_id).exists()
-            if not order_exists:
-                CloudCommerceOrder.create_from_print_queue(print_log)
-                new_order_count += 1
-        print(new_order_count)
+        try:
+            print_logs = CCAPI.get_print_queue()
+            new_order_count = 0
+            for print_log in print_logs:
+                order_exists = CloudCommerceOrder.objects.filter(
+                    order_id=print_log.order_id).exists()
+                if not order_exists:
+                    CloudCommerceOrder.create_from_print_queue(print_log)
+                    new_order_count += 1
+            print(new_order_count, file=sys.stderr)
+        except Exception as e:
+            logger.error(
+                'Admin Command Error: %s',
+                ' '.join(sys.argv),
+                exc_info=sys.exc_info())
+            raise e
