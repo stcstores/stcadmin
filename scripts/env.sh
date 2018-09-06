94337d189c47ee5759580604c5861826fde07e0a
Luke Shiner <luke@lukeshiner.com>
2018-09-06 11:46:57 +0100

    Add template for .env file.

---
+++ scripts/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DOMAIN=""  # The domain of the site. Used in file paths.
+export BRANCH=""  # The site will be updated when changes are commited to this path.
+
+source /home/stcstores/.pythonpaths  # Set path to python executables.
