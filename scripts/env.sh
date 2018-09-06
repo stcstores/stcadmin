d60c6021f49daf0bc81ea6475c6fcbc10a48c760
Luke Shiner <luke@lukeshiner.com>
2018-09-06 13:05:58 +0100

    Tweek scripts.

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-export DOMAIN=""  # The domain of the site. Used in file paths.
-export BRANCH=""  # The site will be updated when changes are commited to this path.
+BRANCH=""  # The site will be updated when changes are commited to this path.
 
-source /home/stcstores/.pythonpaths  # Set path to python executables.
+if [ -z "$BRANCH" ]; then
+  echo "Branch not set."
+  exit 1
+fi
+
+PROJECT_DIR="$(realpath "$(dirname "$(dirname "$0")")")"
+DOMAIN="$(basename $PROJECT_DIR)"  # The domain of the site. Used in file paths.
+export BRANCH
+export PROJECT_DIR
+export DOMAIN
+
+# shellcheck source=/home/$USER/.pythonpaths
+source ~/.pythonpaths  # Set path to python executables.
--- scripts/update.sh
+++ scripts/update.sh
@@ -3,10 +3,11 @@
 # Import local variables.
 # shellcheck source=./env.sh
 source "$(dirname "$(realpath "$0")")/env.sh"
+# Sets DOMAIN, BRANCH, PROJECT DIR and PATH
 
 #Set path variables
-PROJECT_DIR="/home/stcstores/sites/$DOMAIN"
 SOURCE_DIR="$PROJECT_DIR/source"
+GIT_DIR="$SOURCE_DIR/.git"
 SCRIPTS_DIR="$PROJECT_DIR/scripts"
 LOG_DIR="$PROJECT_DIR/logs"
 LOGFILE="$LOG_DIR/update.log"
@@ -22,72 +23,77 @@ if [ ! -z "$1" ]; then
 fi
 NEW_COMMIT="$2"
 
-
 # Create log file. This is done after the branch check so an empty file is not written
 # if a non matching branch is specified.
 DATE=`date '+%Y-%m-%d %H:%M:%S'`
-echo "$DATE" > $LOGFILE #  Clear log file and add the current time and date.
+printf "$DATE\n\n" > $LOGFILE #  Clear log file and add the current time and date.
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>>$LOGFILE 2>&1  # Redirect stdout and stder to log file.
 
-echo "Branch argument: '$1'"
-echo "Commit argument: '$2'"
+# Write current variables to log file.
+printf "Passed branch argument: '$1'\n"
+printf "Passed Commit argument: '$2'\n"
+printf "Project: $PROJECT_DIR\n"
+printf "Gitpath: $GIT_DIR\n"
+printf "Domain: $DOMAIN\n"
+printf "Project branch: $BRANCH\n"
+printf "Default python executable: `which python`\n"
+printf "Current commit: `git --git-dir $GIT_DIR log -n 1 --pretty=format:'%h %s'`\n\n"
 
 # Ensure the environment is built with a predictable path.
 export PIPENV_VENV_IN_PROJECT=true
 
 #Update git repo
-cd $SOURCE_DIR || exit 1
-echo "Cleaning git repo..."
-git fetch
-git clean -f
-git reset --hard HEAD
+printf "\nCleaning git repo...\n"
+git --git-dir $GIT_DIR fetch
+git --git-dir $GIT_DIR clean -f
+git --git-dir $GIT_DIR reset --hard HEAD
 
-echo "Updating git repo..."
+printf "\nUpdating git repo...\n"
 if [ ! -z "$NEW_COMMIT" ]; then
-  echo "Updating $DOMAIN source to commit $NEW_COMMIT"
-  git checkout -f $NEW_COMMIT
+  printf "Updating $DOMAIN source to commit $NEW_COMMIT\n"
+  git --git-dir $GIT_DIR checkout -f $NEW_COMMIT
 else
-  echo "Updating $DOMAIN source to tip of $BRANCH"
-  git checkout -f $BRANCH
+  printf "Updating $DOMAIN source to tip of $BRANCH\n"
+  git --git-dir $GIT_DIR checkout -f "origin/$BRANCH"
 fi
 
-UPDATED_COMMIT=`git log -n 1 --pretty=format:'%h %s'`
-echo "Updated to commit $UPDATED_COMMIT"
+UPDATED_COMMIT=`git --git-dir $GIT_DIR log -n 1 --pretty=format:'%h %s'`
+printf "\nUpdated to commit $UPDATED_COMMIT\n"
 
 #Set up python environment.
-echo "Building environment..."
+printf "\nBuilding environment...\n"
 make production-init
 
 # Quit with error if no config file exists.
 if [ ! -f $CONFIG_FILE ]; then
-  echo "ERROR: Config file does not exist at $CONFIG_FILE. Exiting..."
+  printf "\nERROR: Config file does not exist at $CONFIG_FILE. Exiting...\n"
   exit 1
 fi
 
 # Create a secret key file if none exists.
 if [ ! -f $SECRET_KEY_FILE ]; then
-  echo "WARNING: Secret key file not found at $SECRET_KEY_FILE."
-  echo "Generating a new secret key file..."
+  printf "\nWARNING: Secret key file not found at $SECRET_KEY_FILE.\n"
+  printf "Generating a new secret key file...\n"
   SECRET_KEY=`cat /dev/urandom | tr -dc 'a-zA-Z0-9(){}!$^&' | fold -w 50 | head -n 1`
   echo "SECRET_KEY = \"$SECRET_KEY\"" > $SECRET_KEY_FILE
 fi
 
 # Build the docs.
-echo "Building docs..."
+printf "\nBuilding docs...\n"
 make docs
 
 # Update static files.
-echo "Collecting static files..."
+printf "\nCollecting static files...\n"
 pipenv run python manage.py collectstatic --noinput
 
 # Migrate the database.
-echo "Migrating database..."
+printf "\nMigrating database...\n"
 pipenv run python manage.py migrate --noinput
 
 # Restart the site with the new version.
-echo "Restarting service..."
+printf "\nRestarting service...\n"
 $SCRIPTS_DIR/restart.sh
 
-echo "$DOMAIN is new using commit $UPDATED_COMMIT"
+printf "\nComplete. $DOMAIN is new using commit $UPDATED_COMMIT\n"
