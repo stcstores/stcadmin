00a6a0a38a64e824ba7ed6272377c85a9487ab4b
Luke Shiner <luke@lukeshiner.com>
2019-02-07 16:44:21 +0000

    feat: Show/hide validation errors

--- validation/templatetags/validation_extras.py
+++ validation/templatetags/validation_extras.py
@@ -6,10 +6,17 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=False)
-def validation_table_group(group_name, stats, errors):
+def validation_table_group(group_name, stats, errors, group_name_url=None):
     """Render a validation error table."""
     t = loader.get_template("validation/validation_table_group.html")
-    return t.render({"group_name": group_name, "stats": stats, "errors": errors})
+    return t.render(
+        {
+            "group_name": group_name,
+            "stats": stats,
+            "errors": errors,
+            "group_name_url": group_name_url,
+        }
+    )
 
 
 @register.simple_tag(takes_context=False)
@@ -24,10 +31,3 @@ def validation_stats(stats):
     """Render the stats for a group of validation errors."""
     t = loader.get_template("validation/validation_stats.html")
     return t.render({"stats": stats})
-
-
-@register.simple_tag(takes_context=False)
-def app_group_name(app, group_name):
-    """Render the model name and link for the app error table header."""
-    t = loader.get_template("validation/app_group_name.html")
-    return t.render({"app": app, "group_name": group_name})
