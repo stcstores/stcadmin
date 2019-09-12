9b6a3bb86503af8dc20cdf5ccfa184db5ed5807d
Luke Shiner <luke@lukeshiner.com>
2019-09-12 15:15:41 +0100

    tests: Add tests for teh ProductImage model

---
+++ inventory/tests/test_models/test_product_image.py
@@ -0,0 +1,25 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from inventory import models
+
+from .test_products import SetupSingleProductRange
+
+
+class TestProductImage(SetupSingleProductRange, TestCase):
+    @patch("inventory.models.product_image.CCAPI")
+    def test_update_CC_image_order_method(self, mock_CCAPI):
+        image_IDs = ["380929", "284930", "402928", "2948729"]
+        for i, image_ID in enumerate(image_IDs):
+            models.ProductImage(
+                image_ID=image_ID,
+                product=self.product,
+                filename=image_ID + ".jpg",
+                URL=f"http://image_server.com/{image_ID}.jpg",
+                position=i,
+            ).save()
+        models.ProductImage.update_CC_image_order(self.product)
+        mock_CCAPI.set_image_order.assert_called_once_with(
+            product_id=self.product.product_ID, image_ids=image_IDs
+        )
