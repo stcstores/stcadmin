f194f300057aadad6de0616c557c5baee918ebe1
Luke Shiner <luke@lukeshiner.com>
2019-11-05 13:34:26 +0000

    tests: Add base tests for views

--- inventory/tests/test_views/inventory_view_test.py
+++ inventory/tests/test_views/inventory_view_test.py
@@ -2,6 +2,8 @@ from stcadmin.tests.stcadmin_test import STCAdminTest
 
 
 class InventoryViewTest(STCAdminTest):
+    group_name = "inventory"
+
     @classmethod
     def setUpTestData(cls):
         cls.create_user()
@@ -11,3 +13,6 @@ class InventoryViewTest(STCAdminTest):
         self.user = self.__class__.user
         self.add_group("inventory")
         self.login_user()
+
+    def remove_group(self):
+        super().remove_group(self.group_name)
