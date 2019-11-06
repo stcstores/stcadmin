a7fcfab24d1e28ad55f2c540eb378b60a482715a
Luke Shiner <luke@lukeshiner.com>
2019-11-06 12:11:19 +0000

    refactor: Move the product search view

--- inventory/views/__init__.py
+++ inventory/views/__init__.py
@@ -1,12 +1,19 @@
 """Views for inventory app."""
 
-from .api import *  # NOQA
-from .descriptions import DescriptionsView  # NOQA
-from .images import ImageFormView  # NOQA
-from .locations import LocationFormView  # NOQA
-from .print_barcodes import BarcodePDF, PrintBarcodeLabels  # NOQA
-from .product import ProductView  # NOQA
-from .product_editor import (  # NOQA
+from .api import (
+    DeleteImage,
+    GetNewRangeSKUView,
+    GetNewSKUView,
+    GetStockLevelView,
+    SetImageOrderView,
+    UpdateStockLevelView,
+)
+from .descriptions import DescriptionsView
+from .images import ImageFormView
+from .locations import LocationFormView
+from .print_barcodes import BarcodePDF, PrintBarcodeLabels
+from .product import ProductView
+from .product_editor import (
     AddDropdown,
     AddListingOption,
     AddProductOptionValues,
@@ -28,10 +35,9 @@ from .product_editor import (  # NOQA
     StartEditingProduct,
     StartNewProduct,
 )
-from .product_order import ProductOrderView  # NOQA
-from .productrange import ProductRangeView  # NOQA
-from .productsearch import ProductSearchView  # NOQA
-from .suppliers import (  # NOQA
+from .product_order import ProductOrderView
+from .productrange import ProductRangeView
+from .suppliers import (
     CreateSupplier,
     CreateSupplierContact,
     DeleteSupplierContact,
@@ -40,5 +46,59 @@ from .suppliers import (  # NOQA
     ToggleSupplierActive,
     UpdateSupplierContact,
 )
-from .variations import VariationsView  # NOQA
-from .views import *  # NOQA
+from .variations import VariationsView
+from .views import (
+    CreateBayView,
+    InventoryUserMixin,
+    ProductSearchView,
+    SKUGeneratorView,
+)
+
+__all__ = [
+    "DeleteImage",
+    "GetNewRangeSKUView",
+    "GetNewSKUView",
+    "GetStockLevelView",
+    "SetImageOrderView",
+    "UpdateStockLevelView",
+    "DescriptionsView",
+    "ImageFormView",
+    "LocationFormView",
+    "BarcodePDF",
+    "PrintBarcodeLabels",
+    "ProductView",
+    "AddDropdown",
+    "AddListingOption",
+    "AddProductOptionValues",
+    "Continue",
+    "CreateInitialVariation",
+    "CreateVariation",
+    "DeleteVariation",
+    "DiscardChanges",
+    "EditAllVariations",
+    "EditProduct",
+    "EditRangeDetails",
+    "EditVariation",
+    "EditVariations",
+    "RemoveDropdown",
+    "RemoveProductOptionValue",
+    "SaveChanges",
+    "SetProductOptionValues",
+    "SetupVariations",
+    "StartEditingProduct",
+    "StartNewProduct",
+    "ProductOrderView",
+    "ProductRangeView",
+    "CreateSupplier",
+    "CreateSupplierContact",
+    "DeleteSupplierContact",
+    "Supplier",
+    "Suppliers",
+    "ToggleSupplierActive",
+    "UpdateSupplierContact",
+    "VariationsView",
+    "CreateBayView",
+    "InventoryUserMixin",
+    "ProductSearchView",
+    "SKUGeneratorView",
+]
--- inventory/views/views.py
+++ inventory/views/views.py
@@ -1,7 +1,7 @@
 """Miscellaneous views for inventory."""
 
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
 
@@ -34,3 +34,36 @@ class CreateBayView(InventoryUserMixin, FormView):
             self.request, messages.SUCCESS, "Created bay {}".format(form.new_bay)
         )
         return redirect("inventory:create_warehouse_bay")
+
+
+class ProductSearchView(InventoryUserMixin, FormView):
+    """View for product search page."""
+
+    template_name = "inventory/product_search/search_page.html"
+    form_class = forms.ProductSearchForm
+
+    def get_form_kwargs(self, *args, **kwargs):
+        """Return the kwargs to be passed to the form."""
+        kwargs = super().get_form_kwargs(*args, **kwargs)
+        if "data" in kwargs:
+            kwargs["data"] = kwargs["data"].copy()
+            for key, value in kwargs["initial"].items():
+                if key not in kwargs["data"]:
+                    kwargs["data"][key] = value
+        return kwargs
+
+    def get_initial(self):
+        """Return the initial values for the product search form."""
+        initial = super().get_initial()
+        initial["end_of_line"] = self.form_class.END_OF_LINE_DEFAULT
+        initial["show_hidden"] = False
+        return initial
+
+    def form_valid(self, form):
+        """Process form request and return HttpResponse."""
+        form.save()
+        return render(
+            self.request,
+            self.template_name,
+            {"form": form, "product_ranges": form.ranges},
+        )
