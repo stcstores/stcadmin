adb8f00a6b0e9a9eda2c586ce3051e5db880661f
Luke Shiner <luke@lukeshiner.com>
2020-02-05 14:37:34 +0000

    change: Add __all__ to manifest views __init__ file

--- spring_manifest/views/__init__.py
+++ spring_manifest/views/__init__.py
@@ -1,3 +1,25 @@
 """Views for manifest app."""
 
-from .views import *  # NOQA
+from .views import (
+    CanceledOrdersView,
+    FileManifestView,
+    ManifestListView,
+    ManifestView,
+    OrderExists,
+    SendSecuredMailManifest,
+    SplitOrderView,
+    UpdateManifest,
+    UpdateOrderView,
+)
+
+__all__ = [
+    "CanceledOrdersView",
+    "FileManifestView",
+    "ManifestListView",
+    "ManifestView",
+    "OrderExists",
+    "SendSecuredMailManifest",
+    "SplitOrderView",
+    "UpdateManifest",
+    "UpdateOrderView",
+]
