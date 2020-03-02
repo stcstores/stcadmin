566da30b63f274a304e20a2451953bf61fd069fa
Luke Shiner <luke@lukeshiner.com>
2020-03-02 12:36:06 +0000

    feat: Add model admin to the fnac app

---
+++ fnac/admin.py
@@ -0,0 +1,78 @@
+"""Model admin for the fnac app."""
+
+from django.contrib import admin
+
+from fnac import models
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Model admin for fnac.Category."""
+
+    fields = ("name", "english", "french")
+    list_display = ("__str__", "name", "english", "french")
+    list_editable = ("name",)
+
+
+@admin.register(models.Size)
+class SizeAdmin(admin.ModelAdmin):
+    """Model admin for fnac.Size."""
+
+    fields = ("name",)
+    list_display = ("__str__", "name")
+    list_editable = ("name",)
+
+
+@admin.register(models.FnacRange)
+class FnacRangeAdmin(admin.ModelAdmin):
+    """Model admin for fnac.FnacRange."""
+
+    fields = ("name", "sku", "category")
+    list_display = ("__str__", "name", "sku", "category")
+    list_editable = ("__str__", "name", "sku", "category")
+
+
+@admin.register(models.FnacProduct)
+class FnacProductAdmin(admin.ModelAdmin):
+    """Model admin for fnac.FnacProduct."""
+
+    fields = (
+        "name",
+        "sku",
+        "fnac_range",
+        "barcode",
+        "description",
+        "price",
+        "brand",
+        "colour",
+        "english_size",
+        "french_size",
+        "stock_level",
+        "do_not_create",
+        "created",
+    )
+    list_display = (
+        "__str__",
+        "name",
+        "sku",
+        "fnac_range",
+        "barcode",
+        "description",
+        "price",
+        "brand",
+        "colour",
+        "english_size",
+        "french_size",
+        "stock_level",
+        "do_not_create",
+        "created",
+    )
+
+
+@admin.register(models.Translation)
+class TranslationAdmin(admin.ModelAdmin):
+    """Model admin for fnac.Translation."""
+
+    fields = ("name", "description", "colour")
+    list_display = ("product", "name", "description", "colour")
+    list_editable = ("name", "description", "colour")
