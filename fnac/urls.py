d23bd2b2cd8156a76d35625fa15e0eb2ec61eba7
Luke Shiner <luke@lukeshiner.com>
2020-03-05 15:13:45 +0000

    feat: Add missing inventory info page to fnac

--- fnac/urls.py
+++ fnac/urls.py
@@ -5,4 +5,11 @@ from django.urls import path
 from fnac import views
 
 app_name = "fnac"
-urlpatterns = [path("", views.Index.as_view(), name="index")]
+urlpatterns = [
+    path("", views.Index.as_view(), name="index"),
+    path(
+        "missing_inventory_info/",
+        views.MissingInventoryInfo.as_view(),
+        name="missing_inventory_info",
+    ),
+]
--- fnac/views.py
+++ fnac/views.py
@@ -2,6 +2,7 @@
 
 from django.views.generic.base import TemplateView
 
+from fnac import models
 from home.views import UserInGroupMixin
 
 
@@ -15,3 +16,17 @@ class Index(FnacUserMixin, TemplateView):
     """Index view for the fnac app."""
 
     template_name = "fnac/index.html"
+
+
+class MissingInventoryInfo(FnacUserMixin, TemplateView):
+    """View for displaying products that are not listed on FNAC due to missing inventory info."""
+
+    template_name = "fnac/missing_inventory_info.html"
+
+    def get_context_data(self, *args, **kwargs):
+        """Return template context."""
+        context = super().get_context_data(*args, **kwargs)
+        context[
+            "products"
+        ] = models.FnacProduct.to_create.missing_inventory_information()
+        return context
