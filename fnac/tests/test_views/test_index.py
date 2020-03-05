fa783f6218eed40f7e1c12fbb7c54cfe9902e6ef
Luke Shiner <luke@lukeshiner.com>
2020-03-05 14:11:01 +0000

    tests: Refactor fnac view tests

---
+++ fnac/tests/test_views/test_index.py
@@ -0,0 +1,44 @@
+import pytest
+
+
+@pytest.fixture
+def url():
+    return "/fnac/"
+
+
+@pytest.fixture
+def valid_get_response(valid_get_request, url):
+    return valid_get_request(url)
+
+
+def test_logged_out_get_method(url, logged_in_client):
+    response = logged_in_client.get(url)
+    assert response.status_code == 403
+
+
+def test_logged_out_get(client, url):
+    response = client.get(url)
+    assert response.status_code == 302
+
+
+def test_logged_in_group_get(valid_get_response):
+    assert valid_get_response.status_code == 200
+
+
+def test_logged_in_post(url, logged_in_client):
+    response = logged_in_client.post(url)
+    assert response.status_code == 403
+
+
+def test_logged_out_post(client, url):
+    response = client.post(url)
+    assert response.status_code == 302
+
+
+def test_logged_in_group_post(group_logged_in_client, url):
+    response = group_logged_in_client.post(url)
+    assert response.status_code == 405
+
+
+def test_url_heading(valid_get_response):
+    assert valid_get_response.content.decode("utf8") == "<h1>Hello</h1>"
