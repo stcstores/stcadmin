d6b635f803e5de752ee078c7ce571498eba2c6ae
Luke Shiner <luke@lukeshiner.com>
2020-03-10 15:46:56 +0000

    change: Add base template to fnac views

---
+++ fnac/tests/test_views/test_base.py
@@ -0,0 +1,16 @@
+import pytest
+from django.shortcuts import reverse
+
+PAGES = [
+    reverse("fnac:index"),
+    reverse("fnac:missing_inventory_info"),
+    reverse("fnac:missing_price_size"),
+    reverse("fnac:missing_category"),
+]
+
+
+@pytest.mark.parametrize("page", PAGES)
+def test_page_navigation(page, group_logged_in_client):
+    content = group_logged_in_client.get(page).content.decode("utf8")
+    for link in PAGES:
+        assert link in content
