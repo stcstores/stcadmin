c68d1c6ec8e0bb81eb608a389db7b4ebff6bb87d
Luke Shiner <luke@lukeshiner.com>
2020-03-10 14:25:21 +0000

    tests: Fix inventory update tests not clearing the database

--- fnac/tests/test_models/test_InventoryUpdate.py
+++ fnac/tests/test_models/test_InventoryUpdate.py
@@ -25,6 +25,7 @@ def mock_latest_inventory_file(inventory_file):
 def import_test_inventory(django_db_blocker, mock_latest_inventory_file):
     with django_db_blocker.unblock():
         models.update_inventory()
+        yield
 
 
 @pytest.fixture
@@ -41,14 +42,12 @@ def test_get_inventory_file():
     assert isinstance(models._InventoryUpdate.get_inventory_file(), Table)
 
 
-@pytest.mark.django_db
 def test_fnac_range_is_created(inventory_file):
     expected_skus = sorted(list(set(inventory_file.get_column("RNG_SKU"))))
     existing_skus = sorted(list(models.FnacRange.objects.values_list("sku", flat=True)))
     assert expected_skus == existing_skus
 
 
-@pytest.mark.django_db
 def test_fnac_products_are_created(inventory_file):
     expected_skus = sorted(list(inventory_file.get_column("VAR_SKU")))
     existing_skus = sorted(
@@ -57,27 +56,22 @@ def test_fnac_products_are_created(inventory_file):
     assert expected_skus == existing_skus
 
 
-@pytest.mark.django_db
 def test_fnac_range_name_set(created_fnac_range, inventory_file):
     assert created_fnac_range.name == "Adults Christmas Reindeer Slippers"
 
 
-@pytest.mark.django_db
 def test_fnac_range_category_is_null(created_fnac_range):
     assert created_fnac_range.category is None
 
 
-@pytest.mark.django_db
 def test_fnac_product_name_set(created_fnac_product, inventory_file):
     assert created_fnac_product.name == "Adults Christmas Reindeer Slippers"
 
 
-@pytest.mark.django_db
 def test_fnac_product_barcode_set(created_fnac_product, inventory_file):
     assert created_fnac_product.barcode == "8944099045578"
 
 
-@pytest.mark.django_db
 def test_fnac_product_description_set(created_fnac_product, inventory_file):
     assert created_fnac_product.description == "\n".join(
         [
@@ -107,72 +101,58 @@ def test_fnac_product_description_set(created_fnac_product, inventory_file):
     )
 
 
-@pytest.mark.django_db
 def test_fnac_product_colour_set(created_fnac_product, inventory_file):
     assert created_fnac_product.colour is None
 
 
-@pytest.mark.django_db
 def test_fnac_product_colour_set_when_available():
     assert models.FnacProduct.objects.get(sku="RU0-755-RPU").colour == "Black Trim"
 
 
-@pytest.mark.django_db
 def test_fnac_product_brand_set(created_fnac_product, inventory_file):
     assert created_fnac_product.brand == "Aucun"
 
 
-@pytest.mark.django_db
 def test_fnac_product_english_size_set(created_fnac_product, inventory_file):
     assert created_fnac_product.english_size == "Large (UK 7-8)"
 
 
-@pytest.mark.django_db
 def test_fnac_product_stock_level_set(created_fnac_product, inventory_file):
     assert created_fnac_product.stock_level == 0
 
 
-@pytest.mark.django_db
 def test_fnac_product_image_1_field_set(created_fnac_product, inventory_file):
     assert created_fnac_product.image_1 == "19796021.jpg"
 
 
-@pytest.mark.django_db
 def test_fnac_product_image_2_field_set(created_fnac_product, inventory_file):
     assert created_fnac_product.image_2 == "33125872.jpg"
 
 
-@pytest.mark.django_db
 def test_fnac_product_image_3_field_set(created_fnac_product, inventory_file):
     assert created_fnac_product.image_3 == ""
 
 
-@pytest.mark.django_db
 def test_fnac_product_image_4_field_set(created_fnac_product, inventory_file):
     assert created_fnac_product.image_4 == ""
 
 
-@pytest.mark.django_db
 def test_fnac_product_french_size_is_null(created_fnac_product):
     assert created_fnac_product.french_size is None
 
 
-@pytest.mark.django_db
 def test_fnac_product_price_is_null(created_fnac_product):
     assert created_fnac_product.price is None
 
 
-@pytest.mark.django_db
 def test_fnac_do_not_create_is_false(created_fnac_product):
     assert created_fnac_product.do_not_create is False
 
 
-@pytest.mark.django_db
 def test_fnac_created_is_false(created_fnac_product):
     assert created_fnac_product.created is False
 
 
-@pytest.mark.django_db
 def test_fnac_range_is_updated(created_fnac_range, inventory_file):
     created_fnac_range.name = "Test Name"
     models.update_inventory()
@@ -242,7 +222,6 @@ def test_clean_images(image_field_contents, expected):
     assert models._InventoryUpdate.clean_images(image_field_contents) == expected
 
 
-@pytest.mark.django_db
 def test_update_inventory_accepts_update_file(inventory_file):
     sku = "999-999-999"
     inventory_file[0]["RNG_SKU"] = sku
