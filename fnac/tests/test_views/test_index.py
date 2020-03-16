48d607076b4311f1d697016f00cf8f1d4184e50b
Luke Shiner <luke@lukeshiner.com>
2020-03-16 15:55:15 +0000

    change: Add link to download new product files

--- fnac/tests/test_views/test_index.py
+++ fnac/tests/test_views/test_index.py
@@ -19,6 +19,29 @@ def valid_get_response_content(valid_get_response):
     return valid_get_response.content.decode("utf8")
 
 
+@pytest.fixture
+def products(
+    fnac_range_factory, fnac_product_factory, size_factory, translation_factory
+):
+    products = [
+        fnac_product_factory.create(),
+        fnac_product_factory.create(created=True),
+        fnac_product_factory.create(description=""),
+        fnac_product_factory.create(price=None),
+        fnac_product_factory.create(french_size=None, price=556),
+        fnac_product_factory.create(french_size=None, price=None),
+        fnac_product_factory.create(
+            fnac_range=fnac_range_factory.create(category=None)
+        ),
+        fnac_product_factory.create(do_not_create=True),
+        fnac_product_factory.create(stock_level=0),
+        fnac_product_factory.create(),
+    ]
+    for product in products[1:]:
+        translation_factory.create(product=product)
+    return products
+
+
 def test_logged_out_get_method(url, logged_in_client):
     response = logged_in_client.get(url)
     assert response.status_code == 403
@@ -68,27 +91,15 @@ def test_links_to_translations(valid_get_response_content):
     assert valid_get_response_content.count(reverse("fnac:translations")) == 2
 
 
-@pytest.fixture
-def products(
-    fnac_range_factory, fnac_product_factory, size_factory, translation_factory
+@pytest.mark.django_db
+def test_links_to_new_product_file(products, valid_get_response_content):
+    assert reverse("fnac:new_product_file") in valid_get_response_content
+
+
+def test_does_not_link_to_product_file_if_no_valid_products_exist(
+    valid_get_response_content,
 ):
-    products = [
-        fnac_product_factory.create(),
-        fnac_product_factory.create(created=True),
-        fnac_product_factory.create(description=""),
-        fnac_product_factory.create(price=None),
-        fnac_product_factory.create(french_size=None, price=556),
-        fnac_product_factory.create(french_size=None, price=None),
-        fnac_product_factory.create(
-            fnac_range=fnac_range_factory.create(category=None)
-        ),
-        fnac_product_factory.create(do_not_create=True),
-        fnac_product_factory.create(stock_level=0),
-        fnac_product_factory.create(),
-    ]
-    for product in products[1:]:
-        translation_factory.create(product=product)
-    return products
+    assert reverse("fnac:new_product_file") not in valid_get_response_content
 
 
 @pytest.mark.django_db
