a0b44b6400253ca10e9f7fd25cb2b9a648418988
Luke Shiner <luke@lukeshiner.com>
2020-12-15 11:28:33 +0000

    Set country of origin on product creation

--- product_editor/editor_manager/productcreator.py
+++ product_editor/editor_manager/productcreator.py
@@ -191,6 +191,7 @@ class ProductCreator(ProductSaver):
             kwargs[self.BARCODE], kwargs[self.DESCRIPTION], kwargs[self.VAT_RATE]
         )
         self.update_product(self, product, **kwargs)
+        product.country_of_origin = 60
 
     def get_variation(self, **kwargs):
         """Return variation to be saved."""
