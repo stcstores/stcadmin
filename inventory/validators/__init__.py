d9a841e45355c7f799054fbb2ae9321daf0b0bc4
Luke Shiner <luke@lukeshiner.com>
2022-03-28 14:34:04 +0100

    Remove old inventory validators

--- inventory/validators/__init__.py
+++ inventory/validators/__init__.py
@@ -1,13 +1,9 @@
 """Inventory validators."""
 
-from .bays import BayValidationRunner
 from .product_ranges import ProductRangeValidationRunner
 from .products import ProductValidationRunner
-from .suppliers import SupplierValidationRunner
 
 __all__ = [
-    "BayValidationRunner",
     "ProductRangeValidationRunner",
     "ProductValidationRunner",
-    "SupplierValidationRunner",
 ]
--- inventory/validators/bays/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Validators for bays."""
-from .validation_runners import BayValidationRunner
-
-__all__ = ["BayValidationRunner"]
