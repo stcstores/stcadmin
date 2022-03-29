f5de325af82e6f990d37cf7627c30255c5f89710
Luke Shiner <luke@lukeshiner.com>
2022-03-29 16:01:36 +0100

    Add management commands to create linnworks exports

---
+++ linnworks/management/commands/create_linnworks_inventory_file.py
@@ -0,0 +1,24 @@
+"""Create a linnworks inventory file."""
+
+import logging
+
+from django.core.management.base import BaseCommand
+
+from linnworks.models import LinnworksProductImportFile
+
+logger = logging.getLogger("management_commands")
+
+
+class Command(BaseCommand):
+    """Create a linnworks inventory file."""
+
+    help = """Create a linnworks inventory file."""
+
+    def handle(self, *args, **options):
+        """Create a linnworks inventory file."""
+        try:
+            csv_file = LinnworksProductImportFile.create()
+            print(csv_file.to_string())
+        except Exception as e:
+            logger.exception("Error creating Linnworks Product Import file.")
+            raise e
