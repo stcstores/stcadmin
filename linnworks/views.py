b396c276a7cdf524df54fd6db74b85bce072d99b
Luke Shiner <luke@lukeshiner.com>
2022-04-20 12:54:08 +0100

    Prevent error emails when stock levels are not available

--- linnworks/views.py
+++ linnworks/views.py
@@ -2,7 +2,8 @@
 
 import json
 
-from django.http import JsonResponse
+import linnapi
+from django.http import HttpResponseServerError, JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 
@@ -15,11 +16,15 @@ def get_stock_levels(request):
     """Return the stock level for a product."""
     output = {}
     request_data = json.loads(request.body)
-    for product_id in request_data["product_ids"]:
-        product = get_object_or_404(BaseProduct, pk=product_id)
-        stock_level = StockManager.get_stock_level(product)
-        output[product_id] = stock_level
-    return JsonResponse(output)
+    try:
+        for product_id in request_data["product_ids"]:
+            product = get_object_or_404(BaseProduct, pk=product_id)
+            stock_level = StockManager.get_stock_level(product)
+            output[product_id] = stock_level
+    except linnapi.exceptions.InvalidResponseError:
+        return HttpResponseServerError
+    else:
+        return JsonResponse(output)
 
 
 @csrf_exempt
