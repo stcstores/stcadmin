591f658cd3dd7e9834d7d30581241a82f85aad2d
Luke Shiner <luke@lukeshiner.com>
2022-07-21 11:19:24 +0100

    Refactor image views

--- inventory/urls.py
+++ inventory/urls.py
@@ -67,6 +67,12 @@ inventory_urlpatterns = [
         views.productrange.ChannelLinks.as_view(),
         name="channel_links",
     ),
+    path(
+        "set_image_order/",
+        views.images.SetImageOrderView.as_view(),
+        name="set_image_order",
+    ),
+    path("delete_image/", views.images.DeleteImage.as_view(), name="delete_image"),
 ]
 
 api_urlpatterns = [
@@ -82,8 +88,6 @@ api_urlpatterns = [
         views.UpdateStockLevelView.as_view(),
         name="update_stock_level",
     ),
-    path("set_image_order/", views.SetImageOrderView.as_view(), name="set_image_order"),
-    path("delete_image/", views.DeleteImage.as_view(), name="delete_image"),
     path("new_brand", views.NewBrand.as_view(), name="new_brand"),
     path("new_manufacturer", views.NewManufacturer.as_view(), name="new_manufacturer"),
     path("new_supplier", views.NewSupplier.as_view(), name="new_supplier"),
