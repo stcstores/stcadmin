94673429d900a480907eeacc3c4987ef59feea3c
Luke Shiner <luke@lukeshiner.com>
2023-06-29 12:23:31 +0100

    Update sku generator page

--- inventory/static/scripts/sku_generator.js
+++ inventory/static/scripts/sku_generator.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
-    var variation_sku_box = $('#variation_sku');
+    var sku_input = $('#sku_input');
     var variation_sku_button = $('#get_variation_sku');
-
-    var range_sku_box = $('#range_sku');
     var range_sku_button = $('#get_range_sku');
 
     variation_sku_button.click(function() {
         $.post(new_sku_url, function(response) {
-            variation_sku_box.val(response);
-            variation_sku_box.select();
+            sku_input.val(response);
+            sku_input.select();
         });
     });
 
     range_sku_button.click(function() {
         $.post(new_range_sku_url, function(response) {
-            range_sku_box.val(response);
-            range_sku_box.select();
+            sku_input.val(response);
+            sku_input.select();
         });
     });
 });
