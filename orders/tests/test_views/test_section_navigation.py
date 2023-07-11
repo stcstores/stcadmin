e94a13e8d44de85e79ada4d2dedc00159f43d361
Luke Shiner <luke@lukeshiner.com>
2023-07-11 11:54:27 +0100

    Start converting Orders app to bootstrap

--- orders/tests/test_views/test_section_navigation.py
+++ orders/tests/test_views/test_section_navigation.py
@@ -1,6 +1,7 @@
 import pytest
 from django.shortcuts import reverse
 from django.template.loader import render_to_string
+from requests_html import HTML
 
 
 @pytest.fixture
@@ -8,8 +9,13 @@ def section_navigation_content():
     return render_to_string("orders/section_navigation.html")
 
 
+@pytest.fixture
+def page_html(section_navigation_content):
+    return HTML(html=section_navigation_content)
+
+
 @pytest.mark.parametrize(
-    "name,link",
+    "name,reverse_path",
     [
         ("Orders", "orders:index"),
         ("Charts", "orders:charts"),
@@ -18,6 +24,5 @@ def section_navigation_content():
         ("Refunds", "orders:refund_list"),
     ],
 )
-def test_orders_section_navigation(name, link, section_navigation_content):
-    link_text = f'<a href="{reverse(link)}">{name}</a>'
-    assert link_text in section_navigation_content
+def test_orders_section_navigation(name, reverse_path, page_html):
+    assert page_html.find(f'a[href="{reverse(reverse_path)}"]', containing=name)
