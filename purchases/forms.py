bdfdf56434cdaa6b2838bdb998f7faefcaeb1dcc
Luke Shiner <luke@lukeshiner.com>
2023-08-03 13:53:04 +0100

    Creating purchases updates stock levels

--- purchases/forms.py
+++ purchases/forms.py
@@ -1,10 +1,13 @@
 """Forms for the purchases app."""
 
+import logging
+
 from django import forms
 from django.db.models import Q
 
 from home.models import Staff
 from inventory.models import BaseProduct
+from linnworks.models import StockManager
 from purchases import models
 
 
@@ -43,8 +46,28 @@ class CreatePurchaseForm(forms.Form):
     def save(self):
         """Create a new purchase."""
         product = BaseProduct.objects.get(pk=self.cleaned_data["product_id"])
-        models.Purchase.objects.new_purchase(
+        self.instance = models.Purchase.objects.new_purchase(
             purchased_by=self.cleaned_data["purchaser"],
             product=product,
             quantity=self.cleaned_data["quantity"],
         )
+
+    def update_stock_level(self):
+        """Update the stock level of a product to reflect the created purchase."""
+        try:
+            current_stock_level = StockManager.get_stock_level(self.instance.product)
+            new_stock_level = current_stock_level - self.instance.quantity
+            if new_stock_level < 0:
+                raise Exception("Cannot set stock level below zero.")
+            updated_stock_level = StockManager.set_stock_level(
+                product=self.instance.product,
+                user=self.instance.purchased_by.stcadmin_user,
+                new_stock_level=new_stock_level,
+                change_source="Staff purchase by {{ purchase.purchased_by }}",
+            )
+        except Exception as e:
+            logger = logging.getLogger("django")
+            logger.exception(e)
+            raise
+        else:
+            return updated_stock_level
